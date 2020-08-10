3c8a1f35723247370af5031c5fc65cc37c31eb48
Ganesh <ganeshraja1010@gmail.com>
2020-08-10 18:04:41 +0530

    ups

--- get_props.js
+++ get_props.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const myargs = process.argv;
+const myargs = process.argv.slice(2);
 
 let rawdata = fs.readFileSync('./env/env_props.json');
 let props = JSON.parse(rawdata);
