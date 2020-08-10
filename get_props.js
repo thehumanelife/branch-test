f9644ee923691f6a991de17240b99e36d60c4b4f
Ganesh <ganeshraja1010@gmail.com>
2020-08-10 18:05:31 +0530

    updated

--- get_props.js
+++ get_props.js
@@ -4,5 +4,4 @@ const myargs = process.argv.slice(2);
 
 let rawdata = fs.readFileSync('./env/env_props.json');
 let props = JSON.parse(rawdata);
-console.log(myargs);
-// console.log(props['master'][process.argv[2]]);
+console.log(props['master'][process.argv[2]]);
