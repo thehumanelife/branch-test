a0baf658dd10a68d11fef6283964cd4f6deb284d
Ganesh <ganeshraja1010@gmail.com>
2020-09-08 14:55:13 +0530

    Updated props and env

--- get_props.js
+++ get_props.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
-const myargs = process.argv.slice(2);
 
-let rawdata = fs.readFileSync('./env/env_props.json');
-let props = JSON.parse(rawdata);
-console.log(props['master'][process.argv[2]]);
+let rawData = fs.readFileSync('./env/env_props.json');
+const props = JSON.parse(rawData);
+
+const branch = process.argv[3];
+const propertyName = process.argv[2];
+
+if (branch in props) console.log(props[branch][propertyName]);
+else console.log('');
