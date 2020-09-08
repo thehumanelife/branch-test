7a0beedb5242e1ba43eb9fa5c59f0e232afac1f1
Ganesh <ganeshraja1010@gmail.com>
2020-09-08 14:34:56 +0530

    updated docker creds

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,4 @@
 docker build -t ${DOCKER_IMAGE_TAG} .
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin ${DOCKER_REPO_URL}
 docker push ${DOCKER_IMAGE_TAG}
 docker image rm ${DOCKER_IMAGE_TAG}
--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,2 +1,3 @@
 docker-compose down
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin ${DOCKER_REPO_URL}
 docker-compose up -d
